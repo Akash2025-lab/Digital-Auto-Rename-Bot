b8f8cf4aa3cfef9b193dae219135c988d84ed238
Akash2025-lab <akhashalder74@gmail.com>
2026-03-15 09:57:19 +0530

    Change API credentials for DigitalAutoRenameBot
    Updated API credentials in the DigitalAutoRenameBot class.

--- bot.py
+++ bot.py
@@ -60,9 +60,9 @@ class DigitalAutoRenameBot(Client):
     def __init__(self):
         super().__init__(
             name="DigitalRenameBot",
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
+            api_id=Config.8724723153,
+            api_hash=Config.0ab2955ad5b1a913e220800b5fc5db36,
+            bot_token=Config.8724723153:AAGuMq5t3Mq8egJUAcT96_VMnZEok8gkTZs,
             workers=200,
             plugins={"root": "plugins"},
             sleep_threshold=5,
